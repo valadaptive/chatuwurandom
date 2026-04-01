cafda6766df6d91dc2265b928b6f2f56bd8b42ce
valadaptive <valadaptive@protonmail.com>
2026-04-01 13:43:36 -0400

    Fix base

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import preact from '@preact/preset-vite';
 import {resolve} from 'path';
 
 export default defineConfig({
+    base: '/chatuwurandom/',
     plugins: [preact()],
     resolve: {
         alias: {
